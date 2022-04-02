84f61552416ed785e43c4a93d03ea3035433ab03
NathanDWasTaken <derna541@hotmail.com>
2022-04-02 19:01:28 +0200

    add overlay js

---
+++ billy_overlay.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         April Fools r/starwars_place Allies
+// @namespace    http://tampermonkey.net/
+// @version      3.1
+// @description  Keep the canvas beautiful!
+// @author       oralekin from osu! /r/osuplace
+// @overlay maintainer /u/irate_kalypso /r/starwars_place
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
+// @downloadURL  https://github.com/iratekalypso/r-place-2022/raw/main/better_alliance_overlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/NathanDWasTaken/r-place/main/billy_overlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
