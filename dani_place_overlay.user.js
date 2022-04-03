242fc6a39a5fc1617d205532c163f4be4b4310e1
NathanDWasTaken <derna541@hotmail.com>
2022-04-03 15:32:27 +0200

    add gimp project files of the sketches, add more comments to img_to_overlay, remove unnecessary images

--- dani_place_overlay.user.js
+++ dani_place_overlay.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name         r/place Billy Bot
-// @namespace    http://tampermonkey.net/
-// @version      3.1
-// @description  Keep the canvas beautiful!
-// @author       EvilTaco 
-// @overlay maintainer /u/irate_kalypso /r/starwars_place
-// @match        https://hot-potato.reddit.com/embed*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/NathanDWasTaken/r-place/raw/main/billy_overlay.user.js
-// @downloadURL  https://github.com/NathanDWasTaken/r-place/raw/main/billy_overlay.user.js
-// @grant        none
+// @name                r/place Dani Overlay
+// @namespace           http://tampermonkey.net/
+// @version             1
+// @description         Shows an overlay over the r/place canvas, making it very easy to create and maintain our artwork
+// @author              EvilTaco
+// @match               https://hot-potato.reddit.com/embed*
+// @icon                https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL           https://github.com/NathanDWasTaken/r-place/raw/main/dani_place_overlay.user.js
+// @downloadURL         https://github.com/NathanDWasTaken/r-place/raw/main/dani_place_overlay.user.js
+// @grant               none
 // ==/UserScript==
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
--- img_to_overlay.py
+++ img_to_overlay.py
@@ -1,13 +1,37 @@
 """
-The point of this program is to turn a normal image into an overlay image which can be used for r/place
+The point of this program is to turn a normal image into an overlay image which can be used for r/place.
 
-
+It loads the sketches given in the sketches list and places them on the canvas/overlay, always leaving 2 empty and transparent spaces between each pixel (both horizontally and veritcally)
 """
 from PIL import Image
 import numpy as np
 
 
+
+class Sketch:
+    """
+    Represents a sketch, in this case a sketch is an image that is placed at a certain coordinate
+    """
+    def __init__(self, img_file: str, start_coords: tuple[int, int]) -> None:
+        self.img                    = np.array(Image.open(img_file))
+
+        # The start coords of the image (top left corner)
+        self.start_coords           = start_coords
+        self.start_x, self.start_y  = start_coords
+
+
+
+# Only thing you should have to change for a regular update
+sketches = [
+    Sketch("moderate sketch.png", (1549, 552)),
+]
+
+
+
+
+
 # Constants
+# These variables should not change under normal circumstances
 
 canvas_x, canvas_y = (2000, 1000)
 
@@ -17,15 +41,6 @@ chunk_size = 3
 
 finished_file = "dani_place_overlay.png"
 
-
-
-class Sketch:
-    def __init__(self, img_file: str, start_coords: tuple[int, int]) -> None:
-        self.img                    = np.array(Image.open(img_file))
-
-        # The start coords of the image (top left corner)
-        self.start_coords           = start_coords
-        self.start_x, self.start_y  = start_coords
 
 
 
@@ -41,6 +56,9 @@ def get_canvas():
 
 
 def add_sketch_to_overlay(overlay, sketch: Sketch):
+    """
+    Adds the sketch to the image
+    """
     img                 = sketch.img
     start_x, start_y    = sketch.start_coords
 
@@ -48,26 +66,25 @@ def add_sketch_to_overlay(overlay, sketch: Sketch):
         for x in range(img.shape[1]):
             pixel = img[y, x]
 
-            # Checks whether pixel is nothing or actually has some colour. If any of the 3 rgb values is non-zero, we don't skip (if all of the values are 0, we skip)
-            if not pixel.any():
+            # Check the opacity of the pixel
+            # If the pixel opacity is below 20 (randomly chosen) we ignore the pixel since it's basically invisible
+            if pixel[3] < 20:
                 # Empty pixel, skip
                 continue
 
+            # add the pixel to the overlay, but only in the middle pixel out of the 9 (since the pixel is actually made up of a 3x3 pixel chunk)
             overlay[(start_y + y) * chunk_size + 1, (start_x + x) * chunk_size + 1] = pixel
 
 
 
-sketches = [
-    Sketch("moderate sketch.png", (1549, 552)),
-]
-
-
 overlay_img = get_canvas()
+
 
 
 for sketch in sketches:
     add_sketch_to_overlay(overlay_img, sketch)
 
 
+# Numpy arry to pillow image
 finished_img = Image.fromarray(overlay_img)
 finished_img.save(finished_file)
